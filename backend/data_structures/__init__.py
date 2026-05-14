4660c1703b2455ff5bf30b9f1f05d58f2a122536
Fiurer7kp <jhonjhoncoral@hotmail.com>
2026-05-14 18:03:15 -0500

    Phase 3: Add data structures package initialization

---
+++ backend/data_structures/__init__.py
@@ -0,0 +1,14 @@
+"""
+Data Structures Package
+
+This module contains all the manually implemented data structures
+for the Library Management System.
+"""
+
+from .simple_list import SimpleList
+from .double_list import DoubleList
+
+__all__ = [
+    'SimpleList',
+    'DoubleList'
+]
